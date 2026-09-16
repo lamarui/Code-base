25bb70a3955b9f9f4b605d0d8415d650c8b8e524
Your Name <you@example.com>
2026-09-16 04:20:01 -0700

    Regular saving

--- javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart } from '../../data/cart.js'
+import { addToCart, cart } from '../../data/cart.js'
 
 describe('test suite: addToCart', () => {
     it('adds an existing product to the cart', () => {
@@ -6,6 +6,10 @@ describe('test suite: addToCart', () => {
     });
 
     it('adds a new product to the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([]);
+        });
+        console.log(localStorage.getItem('cart'))
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart.length).toEqual(1);
     })
