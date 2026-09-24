bfb11645f14613c1ba3630ef92003d95702f3ec9
Your Name <you@example.com>
2026-09-24 04:32:17 -0700

    Changes

--- javascript-amazon-project-main copy 3/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main copy 3/javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,7 @@
+import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 // import '../data/cart-oop.js'
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
